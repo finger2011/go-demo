9ec5050f975192f94e60c9b8eed3485f46a8b083
qiqizhang-abukito <zhangqiqi@abukito.com>
2021-09-15 19:58:58 +0800

    leetcode 255 a better solution

--- leetcode/main.go
+++ leetcode/main.go
@@ -6,6 +6,6 @@ import (
 
 func main()  {
 	fmt.Println("start:")
-	var order = []int{1,3,4,2}
-	fmt.Printf("result:%v", verifyPreorder(order))
+	var order = []int{3,1,4,2}
+	fmt.Printf("result:%v", verifyPreorder2(order))
 }
