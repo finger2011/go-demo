e589a43329270f0b2f3a5ee0ee87d5c0de36c08c
qiqizhang-abukito <zhangqiqi@abukito.com>
2021-09-18 22:46:32 +0800

    signal control, add timeout

--- third_week/web/shutdown.go
+++ third_week/web/shutdown.go
@@ -1,28 +1,41 @@
 package web
 
 import (
+	"context"
 	"fmt"
-	"syscall"
-	"os/signal"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 //ShutDownPrepare 关闭server
-func ShutDownPrepare()  {
-	
+//步骤
+//1 下线服务
+//2 拒绝请求
+//3 请求执行完毕
+//4 关闭进程
+//5 超时直接关闭
+func ShutDownPrepare() {
+
 }
 
 //ShutDown shut down
-func ShutDown()  {
+func ShutDown() {
 	ch := make(chan os.Signal)
 	//监听指定信号
 	signal.Notify(ch, syscall.SIGKILL, syscall.SIGINT)
 
-	go func(){
-		for s:= range ch {
+	go func() {
+		for s := range ch {
 			switch s {
 			case syscall.SIGKILL, syscall.SIGINT:
-				fmt.Println("Program Exit...")
+				fmt.Printf("get %s signal, Program Exit...\n", s)
+				//超时控制，超过1min未成功关闭，强制退出
+				time.AfterFunc(time.Minute, func() {
+					fmt.Println("shut down timeout...")
+					os.Exit(-1)
+				})
 				GracefllExit()
 			}
 		}
@@ -30,9 +43,11 @@ func ShutDown()  {
 }
 
 //GracefllExit gracefll exit
-func GracefllExit()  {
+func GracefllExit() {
 	fmt.Println("Start Exit")
+	_, cancel := context.WithTimeout(context.Background(), time.Second*30)
 	fmt.Println("Clean...")
+	cancel()
 	fmt.Println("End Exit")
 	os.Exit(0)
 }
