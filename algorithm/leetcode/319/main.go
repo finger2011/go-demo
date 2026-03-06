64a95f6341d65555b6138f6f29f195b54286b983
finger2011 <c3663916@gmail.com>
2026-03-06 16:26:55 +0800

    add注释

--- algorithm/leetcode/319/main.go
+++ algorithm/leetcode/319/main.go
@@ -13,6 +13,7 @@ func main() {
 // 只有完全平方数才会亮着
 // 判断第i灯泡是否亮着，取决于i的约数的个数，只有奇数个数的约数才会亮着
 // 有奇数个数约数的i只有完全平方数：因为如果存在x是i的约数，那么i/x也是i的约数，这时只有x = i/x时，才有奇数个约数
+// int自动向下取整
 func bulbSwitch(n int) int {
 	return int(math.Sqrt(float64(n)))
 }
