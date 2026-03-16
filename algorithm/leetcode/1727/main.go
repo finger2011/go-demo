49f3c070c54338d4ec779ee9cb646de1e7ee4be2
finger2011 <c3663916@gmail.com>
2026-03-17 02:05:41 +0800

    comment

--- algorithm/leetcode/1727/main.go
+++ algorithm/leetcode/1727/main.go
@@ -13,6 +13,8 @@ func main() {
 // 0 0 1		 1 0  0
 // 1 1 1   =>	[1 1] 1
 // 1 0 1		[1 1] 0
+// 1.枚举矩形的底，即从i,j往上全1是的个数，如果grid[i][j] = 0,那么无法作为矩形的底
+// 2. 贪心的把全是1的底放在一起，那么最大矩形就是底长度 * 最小高度
 func largestSubmatrix(matrix [][]int) int {
 	var ans int
 	n := len(matrix[0])
@@ -27,7 +29,11 @@ func largestSubmatrix(matrix [][]int) int {
 		}
 		hs := slices.Clone(height)
 		slices.Sort(hs)
+		// 底的长度不同，高度也不同
 		for i, h := range hs {
+			if h == 0 {
+				continue
+			}
 			ans = max(ans, (n-i)*h)
 		}
 	}
