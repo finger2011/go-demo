44e792769156124df8d5e7aa4948bd28ae8fab03
finger2011 <c3663916@gmail.com>
2026-03-22 06:58:43 +0800

    1886

--- algorithm/leetcode/1886/main.go
+++ algorithm/leetcode/1886/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
-	mat := [][]int{}
-	target := [][]int{}
+	mat := [][]int{{0, 0, 0}, {0, 1, 0}, {1, 1, 1}}
+	target := [][]int{{1, 1, 1}, {0, 1, 0}, {0, 0, 0}}
 
 	fmt.Println("mat:", mat)
 	fmt.Println("target:", target)
@@ -12,5 +15,25 @@ func main() {
 }
 
 func findRotation(mat [][]int, target [][]int) bool {
-	return true
+	for range 4 {
+		if slices.EqualFunc(mat, target, slices.Equal[[]int]) {
+			return true
+		}
+		rotate(mat)
+	}
+	return false
+}
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	// 转置
+	for i := range n {
+		for j := range i {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	// 行翻转
+	for _, row := range matrix {
+		slices.Reverse(row)
+	}
 }
