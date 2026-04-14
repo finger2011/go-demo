0743d52497a9b6f0b86533de193dc0d7393f454f
finger2011 <c3663916@gmail.com>
2026-04-15 05:33:19 +0800

    2515

---
+++ algorithm/leetcode/2515/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Input struct {
+	words      []string
+	target     string
+	startIndex int
+	except     int
+}
+
+func main() {
+	tests := []*Input{
+		{
+			words:      []string{"hello", "i", "am", "leetcode", "hello"},
+			target:     "hello",
+			startIndex: 1,
+			except:     1,
+		},
+	}
+	for _, test := range tests {
+		fmt.Println("======================start")
+		fmt.Println("words:", test.words)
+		fmt.Println("target:", test.target)
+		fmt.Println("startIndex:", test.startIndex)
+		ans := closestTarget(test.words, test.target, test.startIndex)
+		fmt.Println("return:", ans)
+		fmt.Println("result:", ans == test.except)
+		fmt.Println("======================end")
+	}
+}
+
+func closestTarget(words []string, target string, startIndex int) int {
+	n, ans := len(words), -1
+	for i := range n/2 + 1 {
+		if words[(startIndex+i)%n] == target || words[(startIndex-i+n)%n] == target {
+			ans = i
+			break
+		}
+	}
+	return ans
+}
