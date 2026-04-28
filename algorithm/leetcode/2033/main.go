e3e176e1b9f9da67fc932acc38566b7a39dae8b6
finger2011 <c3663916@gmail.com>
2026-04-28 14:03:52 +0800

    中位数定理

--- algorithm/leetcode/2033/main.go
+++ algorithm/leetcode/2033/main.go
@@ -11,7 +11,33 @@ func main() {
 	fmt.Println("minOperations:", minOperations(grid, x))
 }
 
+// 中位数定理：https://zhuanlan.zhihu.com/p/1922938031687595039
 func minOperations(grid [][]int, x int) int {
+	var ans int
+	m, n := len(grid), len(grid[0])
+	total := m * n
+	pos := make([]int, 0, total)
+	for i := range grid {
+		for j := range grid[i] {
+			if (grid[i][j]-grid[0][0])%x != 0 {
+				return -1
+			}
+		}
+		pos = append(pos, grid[i]...)
+	}
+	slices.Sort(pos)
+	media := pos[total/2]
+	for _, num := range pos {
+		if num >= media {
+			ans += num - media
+		} else {
+			ans += media - num
+		}
+	}
+	return ans / x
+}
+
+func minOperations2(grid [][]int, x int) int {
 	var ans int
 	m, n := len(grid), len(grid[0])
 	total := m * n
