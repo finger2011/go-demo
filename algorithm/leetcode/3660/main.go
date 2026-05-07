62cde09f069585891279bff8402c1a5a64e1cf8e
finger2011 <c3663916@gmail.com>
2026-05-07 23:33:13 +0800

    3660

---
+++ algorithm/leetcode/3660/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+func main() {
+
+}
+
+func maxValue(nums []int) []int {
+	n := len(nums)
+	preMax := make([]int, n)
+	preMax[0] = nums[0]
+	for i := 1; i < n; i++ {
+		preMax[i] = max(preMax[i-1], nums[i])
+	}
+
+	ans := make([]int, n)
+	sufMin := math.MaxInt
+	for i := n - 1; i >= 0; i-- {
+		if preMax[i] <= sufMin {
+			ans[i] = preMax[i]
+		} else {
+			ans[i] = ans[i+1]
+		}
+		sufMin = min(sufMin, nums[i])
+	}
+	return ans
+}
