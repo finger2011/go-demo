a7048aa8d9baad8af99dc2bdf6fefe058c7c0e29
finger2011 <c3663916@gmail.com>
2026-05-08 23:16:10 +0800

    3629

---
+++ algorithm/leetcode/3629/main.go
@@ -0,0 +1,57 @@
+package main
+
+func main() {
+
+}
+
+const mx = 1_000_001
+
+var primeFactors = [mx][]int{}
+
+func initPrime() {
+	// 预处理每个数的质因子列表，思路同埃氏筛
+	for i := 2; i < mx; i++ {
+		if primeFactors[i] == nil { // i 是质数
+			for j := i; j < mx; j += i { // i 的倍数有质因子 i
+				primeFactors[j] = append(primeFactors[j], i)
+			}
+		}
+	}
+}
+
+func minJumps(nums []int) (ans int) {
+	n := len(nums)
+	initPrime()
+	groups := map[int][]int{}
+	for i, x := range nums {
+		for _, p := range primeFactors[x] {
+			groups[p] = append(groups[p], i) // 对于质数 p，可以跳到下标 i
+		}
+	}
+
+	vis := make([]bool, n)
+	vis[0] = true
+	q := []int{0}
+
+	for ; ; ans++ {
+		tmp := q
+		q = nil
+		for _, i := range tmp {
+			if i == n-1 {
+				return
+			}
+			idx := groups[nums[i]]
+			idx = append(idx, i+1)
+			if i > 0 {
+				idx = append(idx, i-1)
+			}
+			for _, j := range idx { // 可以从 i 跳到 j
+				if !vis[j] {
+					vis[j] = true
+					q = append(q, j)
+				}
+			}
+			delete(groups, nums[i]) // 避免重复访问下标列表
+		}
+	}
+}
