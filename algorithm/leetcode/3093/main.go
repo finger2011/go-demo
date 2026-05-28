7ee5d415a62a8f22b77fc77b6f770ee10110844c
finger2011 <c3663916@gmail.com>
2026-05-28 23:25:54 +0800

    3093

---
+++ algorithm/leetcode/3093/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "math"
+
+func main() {
+
+}
+
+func stringIndices(wordsContainer, wordsQuery []string) []int {
+	type node struct {
+		children [26]*node
+		minLen   int // 子树中的最短字符串的长度
+		idx      int // 子树中的最短字符串的下标
+	}
+	root := &node{minLen: math.MaxInt}
+
+	for i, s := range wordsContainer {
+		l := len(s)
+		if l < root.minLen {
+			root.minLen = l
+			root.idx = i
+		}
+
+		// 把 reverse(s) 插入字典树
+		cur := root
+		for j := l - 1; j >= 0; j-- {
+			b := s[j] - 'a'
+			if cur.children[b] == nil {
+				cur.children[b] = &node{minLen: math.MaxInt}
+			}
+			cur = cur.children[b]
+			// 维护 cur 子树中的最短字符串的长度及其下标
+			// 由于我们是按照 i 从小到大的顺序遍历，字符串长度相同时不更新 idx
+			if l < cur.minLen {
+				cur.minLen = l
+				cur.idx = i
+			}
+		}
+	}
+
+	ans := make([]int, len(wordsQuery))
+	for i, s := range wordsQuery {
+		cur := root
+		for j := len(s) - 1; j >= 0 && cur.children[s[j]-'a'] != nil; j-- {
+			cur = cur.children[s[j]-'a']
+		}
+		// 退出循环时，cur 即最长公共前缀（的对应节点），cur.idx 是前缀为 cur 的最短字符串的下标
+		ans[i] = cur.idx
+	}
+	return ans
+}
