25ad9f5055bf521f440b7a8cce0f0bbd4dfc25a3
finger2011 <c3663916@gmail.com>
2026-06-04 12:11:08 +0800

    3751

---
+++ algorithm/leetcode/3751/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	num1, num2 := 120, 130
+	fmt.Println("num:", totalWaviness(num1, num2))
+}
+
+func totalWaviness(num1 int, num2 int) int {
+	var ans int
+	for i := num1; i <= num2; i++ {
+		if i < 100 {
+			continue
+		}
+		num := i
+		f1 := num % 10
+		num /= 10
+		f2 := num % 10
+		num /= 10
+		for num > 0 {
+			f3 := num % 10
+			num /= 10
+			if (f2 > f1 && f2 > f3) || (f2 < f1 && f2 < f3) {
+				ans++
+			}
+			f1, f2 = f2, f3
+		}
+	}
+	return ans
+}
