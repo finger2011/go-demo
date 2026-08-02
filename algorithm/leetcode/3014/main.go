b62fde06ea1f69cf6ac39fce7a527a39596c92e1
finger2011 <c3663916@gmail.com>
2026-08-02 21:47:59 +0800

    进阶

--- algorithm/leetcode/3014/main.go
+++ algorithm/leetcode/3014/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "slices"
+
 func main() {
 
 }
@@ -11,4 +13,21 @@ func minimumPushes(word string) int {
 	}
 	return ans
 
+}
+
+// 进阶：如果有相同字母
+func minimumPushes2(word string) int {
+	chars := make([]int, 26)
+	for _, ch := range word {
+		chars[ch-'a']++
+	}
+	slices.SortFunc(chars, func(a, b int) int {
+		return b - a
+	})
+	var ans int
+	for i, num := range chars {
+		k := (i + 8) / 8
+		ans += k * num
+	}
+	return ans
 }
